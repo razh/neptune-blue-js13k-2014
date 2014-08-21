29f1688ac4f823b736a82be34d88a9a78728b7e9
Raymond Zhou <somecrazyidiot@gmail.com>
2014-08-21 01:49:12 -0400

    Add Material draw() method.
    Update defaults and add Side “enum”.

--- src/js/materials/material.js
+++ src/js/materials/material.js
@@ -1,27 +1,57 @@
 'use strict';
 
+var Color = require( '../math/color' );
+
+var FrontSide  = 0,
+    BackSide   = 1,
+    DoubleSide = 2;
+
 function Material( options ) {
   options = options || {};
 
-  this.batch = options.batch || false;
+  this.batch = ( options.batch !== undefined ) ? options.batch : true;
+  this.side = options.side || FrontSide;
 
-  this.alpha = options.alpha || 0;
-  this.fill = options.fill || 0;
-  this.stroke = options.stroke || 0;
+  this.color = options.color || new Color();
+  this.opacity = ( options.opacity !== undefined ) ? options.opacity : 1;
   this.lineWidth = options.lineWidth || 0;
 
-  this.shadowColor = options.shadowColor || 0;
+  this.fillVisible = ( options.fillVisible !== undefined ) ? options.fillVisible : true;
+  this.wireframe = ( options.wireframe !== undefined ) ? options.wireframe : true;
+
+  this.shadowColor = options.shadowColor || new Color();
   this.shadowBlur = options.shadowBlur || 0;
+
+  this.blending = options.blending || 'source-over';
 }
 
 Material.prototype.set = function( ctx ) {
-  ctx.globalAlpha = this.alpha;
-  ctx.fillStyle = this.fill;
-  ctx.strokeStyle = this.stroke;
+  var color = this.color.toString();
+
+  ctx.fillStyle = color;
+  ctx.strokeStyle = color;
+
+  ctx.globalAlpha = this.opacity;
   ctx.lineWidth = this.lineWidth;
 
-  ctx.shadowColor = this.shadowColor;
+  ctx.shadowColor = this.shadowColor.toString();
   ctx.shadowBlur = this.shadowBlur;
+
+  ctx.globalCompositeOperation = this.blending;
 };
+
+Material.prototype.draw = function( ctx ) {
+  if ( this.fillVisible ) {
+    ctx.fill();
+  }
+
+  if ( this.wireframe ) {
+    ctx.stroke();
+  }
+};
+
+Material.FrontSide = FrontSide;
+Material.BackSide = BackSide;
+Material.DoubleSide = DoubleSide;
 
 module.exports = Material;
