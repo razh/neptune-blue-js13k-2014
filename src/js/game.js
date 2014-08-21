1a7803878f5f6ed1659c4c3f4ab8c0ce515ecf6d
Raymond Zhou <somecrazyidiot@gmail.com>
2014-08-21 03:08:36 -0400

    Add lights, camera, rendering to Game class.

--- src/js/game.js
+++ src/js/game.js
@@ -1,7 +1,15 @@
 /*global requestAnimationFrame*/
 'use strict';
 
-function Game() {
+var Color = require( './math/color' );
+var Camera = require( './camera' );
+var DirectionalLight = require( './lights/directional-light' );
+var Renderer = require( './renderer/renderer' );
+
+function Game( width, height ) {
+  width  = width  || window.innerWidth;
+  height = height || window.innerHeight;
+
   this.canvas = document.createElement( 'canvas' );
   this.ctx = this.canvas.getContext( '2d' );
 
@@ -9,7 +17,20 @@ function Game() {
   this.prevTime = 0;
   this.currTime = 0;
 
+  this.fov = 90;
+  this.camera = new Camera( this.fov );
+  this.setSize( width, height );
+
   this.entities = [];
+
+  this.light = new DirectionalLight();
+  this.ambient = new Color();
+
+  this.renderer = new Renderer({
+    ctx: this.ctx,
+    light: this.light,
+    ambient: this.ambient
+  });
 
   this.tick = this.tick.bind( this );
 }
@@ -41,11 +62,7 @@ Game.prototype.update = function() {
 };
 
 Game.prototype.draw = function() {
-  var ctx = this.ctx;
-
-  this.entities.forEach(function( entity ) {
-    entity.draw( ctx );
-  });
+  this.renderer.render( this.entities, this.camera );
 };
 
 Game.prototype.play = function() {
@@ -56,6 +73,13 @@ Game.prototype.play = function() {
 
 Game.prototype.pause = function() {
   this.running = false;
+};
+
+Game.prototype.setSize = function( width, height ) {
+  this.canvas.width  = width;
+  this.canvas.height = height;
+  this.camera.aspect = width / height;
+  this.camera.updateProjectionMatrix();
 };
 
 module.exports = Game;
