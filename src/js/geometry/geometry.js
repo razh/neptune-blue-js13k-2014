2456021fe70f1b4d1161c0fc3ecbe2249dfd2483
Raymond Zhou <somecrazyidiot@gmail.com>
2014-08-29 03:20:00 -0400

    Add Geometry push() method for batch vertex/face addition.

--- src/js/geometry/geometry.js
+++ src/js/geometry/geometry.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var Vector3 = require( '../math/vector3' );
+var Face3 = require( './face3' );
+var Quad = require( './quad' );
 
 // Temp vectors.
 var cb = new Vector3(),
@@ -10,6 +12,64 @@ function Geometry() {
   this.vertices = [];
   this.faces = [];
 }
+
+/**
+ * Appends the geometry defined by a flat array of vertex data and a 2D array
+ * of face indices.
+ *
+ * faces is an array of indices in the format:
+ *
+ *   [
+ *     // Face.
+ *     [ 0, 1, 2 ],
+ *     // Quad.
+ *     [ 3, 4, 5, 6 ]
+ *     // Face.
+ *     [ 7, 8, 9 ]
+ *   ]
+ *
+ */
+Geometry.prototype.push = function( vertices, faces ) {
+  var offset = this.vertices.length;
+
+  var i, il;
+  for ( i = 0, il = vertices.length; i < il; i += 3 ) {
+    this.vertices.push(
+      new Vector3(
+        vertices[ i     ],
+        vertices[ i + 1 ],
+        vertices[ i + 2 ]
+      )
+    );
+  }
+
+  var indices;
+  var face;
+  for ( i = 0, il = faces.length; i < il; i++ ) {
+    indices = faces[i];
+
+    if ( indices.length === 3 ) {
+      face = new Face3(
+         offset + indices[ 0 ],
+         offset + indices[ 1 ],
+         offset + indices[ 2 ]
+      );
+    } else if ( indices.length === 4 ) {
+      face = new Quad(
+         offset + indices[ 0 ],
+         offset + indices[ 1 ],
+         offset + indices[ 2 ],
+         offset + indices[ 3 ]
+      );
+    } else {
+      continue;
+    }
+
+    this.faces.push( face );
+  }
+
+  return this;
+};
 
 Geometry.prototype.computeFaceNormals = function() {
   var face;
