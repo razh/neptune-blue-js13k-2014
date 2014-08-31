1a2d66da2c42f1308ee6fe276ec0839f108da046
Raymond Zhou <somecrazyidiot@gmail.com>
2014-08-31 02:04:52 -0400

    Extract Polyhedron test from main.js.

--- test/main.js
+++ test/main.js
@@ -4,3 +4,4 @@ require( './building/js/building-test' );
 require( './track/js/track-test' );
 require( './waves/js/waves-test' );
 require( './ship/js/ship-test' );
+require( './polyhedron/js/polyhedron-test' );
