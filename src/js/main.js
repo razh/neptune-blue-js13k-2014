baa6e4471ac064272ccb747654291987d86e3d93
Raymond Zhou <somecrazyidiot@gmail.com>
2014-08-31 20:11:06 -0400

    Add DOM manipulation utility functions.

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,47 @@ var LambertGlowMaterial = require( './materials/lambert-glow-material' );
 var Mesh = require( './objects/mesh' );
 var DirectionalLight = require( './lights/directional-light' );
 
+var $ = document.querySelector.bind( document );
+
+function on( el, type, listener ) {
+  el.addEventListener( type, listener );
+}
+
+function off( el, type, listener ) {
+  el.removeEventListener( type, listener );
+}
+
+function create( type ) {
+  return document.createElement( type );
+}
+
+function append( parent, el ) {
+  parent.appendChild( el );
+}
+
+function prepend( parent, el ) {
+  parent.insertBefore( el, parent.firstChild );
+}
+
+function remove( el ) {
+  if ( el.parentNode ) {
+    el.parentNode.removeChild( el );
+  }
+}
+
+function textContent( el, text ) {
+  el.textContent = text;
+}
+
+function addClass( el, className ) {
+  el.classList.add( className );
+}
+
+function removeClass( el, className ) {
+  el.classList.remove( className );
+}
+
+
 var game = new Game(
   Math.min( window.innerWidth, 568 ),
   Math.min( window.innerHeight, 320 )
